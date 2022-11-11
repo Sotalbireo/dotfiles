afd29189eb5b65b72895a0e4d7abceb56ee1de61
Kyon Sasaki <athene609.cyl@gmail.com>
2022-11-11 02:13:23 +0000

    update

--- init/setup/link.sh
+++ init/setup/link.sh
@@ -6,12 +6,12 @@ mkdir -p ~/.ssh
 ln -sf ~/dotfiles/.ssh/config ~/.ssh/config
 
 if test "$(uname)" = 'Darwin'; then
-  ln -sf ~/dotfiles/mac/.bashrc ~/.bashrc
+  ln -sf ~/dotfiles/bash/mac/.bashrc ~/.bashrc
   mkdir -p ~/.1password
   ln -s ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.ssh/agent.sock
 fi
 
 
 if is_wsl; then
-  ln -sf ~/dotfiles/wsl/.bashrc ~/.bashrc
+  ln -sf ~/dotfiles/bash/wsl/.bashrc ~/.bashrc
 fi
